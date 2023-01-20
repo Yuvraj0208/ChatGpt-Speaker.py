4d9f2f84b563d9a12438b89e71b2efe6bae0d9f1
Yuvraj0208 <syuvraj61@gmail.com>
2023-01-20 15:55:39 +0530

    Updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,10 +2,16 @@ import openai
 import speech_recognition as sr
 import pyttsx3
 
+# API key for OpenAI
 openai.api_key = "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"
+
+# Model engine to use for response generation
 model_engine = "text-davinci-002"
 
 def generate_response(prompt):
+    """
+    Generates a response for the given prompt using the OpenAI API.
+    """
     completions = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
@@ -18,31 +24,42 @@ def generate_response(prompt):
     return message
 
 def listen():
+    """
+    Listens for audio input and returns a text transcript of the spoken words.
+    """
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
     try:
-        command = r.recognize_google(audio)
-        return command
+        result2 = r.recognize_google(audio, show_all=True)
+        if result2["final"]:
+            command = result2["alternative"][0]["transcript"]
+            return command
+        else:
+            print("Could not understand audio")
     except sr.UnknownValueError:
         print("Could not understand audio")
     except sr.RequestError as e:
         print("Error requesting results from Google Speech Recognition service; {0}".format(e))
 
 def speak(text):
+    """
+    Speaks the given text using the pyttsx3 library.
+    """
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
 def run_smart_speaker():
+    """
+    Runs the smart speaker loop, listening for audio input and generating a response.
+    """
     while True:
         command = listen()
         response = generate_response(command)
+        print(response)
         speak(response)
+
+# Start listening for audio input
 print("listening...")
-print(speak)
 run_smart_speaker()
-
-
-
-
